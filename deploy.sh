cd5bf6c2ee3e067df60d466bc9ea1a0f5e86a391
Ingyu Yoon <loveloper44@gmail.com>
2019-10-15 14:01:21 +0900

    Refactor: deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
+#!/usr/bin/env bash
+
+NEW_VERSION=$1 
+
+if [ -z "$NEW_VERSION" ]; then
+    echo "new version is required [major, minor, patch, ...]"
+    exit 1
+fi
+
 npm run build
 
-npm version patch
+npm version $NEW_VERSION
 
 npm run deploy
